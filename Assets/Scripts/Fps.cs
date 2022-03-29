b6423641d5856b3c272c00b85059cc9727974809
Keliorw <miabrilo228@gmail.com>
2022-03-29 12:30:33 +0300

    Fps Show

---
+++ Assets/Scripts/Fps.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fps : MonoBehaviour
+{
+
+    public static float fps;
+    public Text FpsText;
+    private void Awake() 
+    {
+        Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 0;   
+    }
+
+    private void OnGUI() 
+    {
+        fps = 1.0f / Time.deltaTime;
+        FpsText.text = "FPS: " + ((int)fps).ToString();
+        if(fps <= 35)
+        {
+            Debug.Log(fps);
+        }
+    }
+}
