bc459b31d4f595d5080b4682adbb27ed964abff3
Sh9pka <Kritffler@gmail.com>
2022-04-04 21:11:44 +0300

    fix push bug

--- Assets/Scripts/Fps.cs
+++ Assets/Scripts/Fps.cs
@@ -5,11 +5,6 @@ using UnityEngine.UI;
 
 public class Fps : MonoBehaviour
 {
-<<<<<<< HEAD:Assets/Game/Scripts/Fps.cs
-=======
-
-    public static float fps;
->>>>>>> 26e3b74d28d9e01bfdc3f19ab614095de04b6681:Assets/Scripts/Fps.cs
     private void Awake() 
     {
         QualitySettings.vSyncCount = 0;
